0b05e08097913bd91e9e617132345d9fe88b1bb2
almat101 <sandruozzo@gmail.com>
2024-12-04 10:42:31 +0100

    create django models

---
+++ django_lvl2/news/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+# a models are used to define database structure as python classes
+
+class Journalist(models.Model):
+	"""generic model of a journalist"""
+	first_name = models.CharField(max_length=30)
+	last_name = models.CharField(max_length=30)
+
+	def __str__(self):
+		return f"{self.first_name} {self.last_name}"
+
+
+class Article(models.Model):
+	"""Generic model of a news articles"""
+	title = models.CharField(max_length=100)
+	content = models.TextField()
+	"""	relational field
+		foreignKey is used to estabilish a relation between the journalist and the article
+		the relation is many-to-one(many articles written by one journalist)
+		this relation field requires 2 position arguments: the class to which the model is related and the on_delete method
+		with on_delete=models.CASCADE if we delete the journalist all the articles (written by that jornalist) are deleted
+		releted_name define the name of the reverse relationship.
+		for example we can retrive a specific article of the journalist with journalist.articles
+	"""
+	journalist = models.ForeignKey(Journalist, on_delete=models.CASCADE, related_name="articles")
+
+	def __str__(self):
+		return self.title
---
+++ venv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#create the virtual enviroment venv
+python3 -m venv venv
+
+#activate the virtual environment
+source venv/bin/activate
+
+pip install django
+
