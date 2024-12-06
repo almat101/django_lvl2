247b267bf48d2693cc100174cdbb0b501e3df33a
almat101 <sandruozzo@gmail.com>
2024-12-06 11:31:47 +0100

    Add Template to render an html page directly
    Template are html files with special syntax that allow you to dynamically generate html content

--- django_lvl2/news/views.py
+++ django_lvl2/news/views.py
@@ -5,22 +5,31 @@ from news.models import Article, Journalist
 # Create your views here.
 
 def home(request):
-	articles_title =[]
-	articles_content =[]
-	journalists =[]
+	articles = Article.objects.all() # this is return a queryset
+	journalists = Journalist.objects.all()
+	context = {"articles" : articles, "journalists" : journalists}
+	return render(request, "news/homepage.html" , context)
 
-	for a in Article.objects.all():
-		articles_title.append("<br>")
-		articles_title.append(a.title)
-		articles_content.append("<br>")
-		articles_content.append(a.content)
 
-	for j in Journalist.objects.all():
-		journalists.append("<br>")
-		journalists.append(j.first_name +" " + j.last_name)
 
-	response = "<h3>Article titles are: </h3>" + " ".join(articles_title) + "<h3>Contents are: </h3>"  +  "".join(articles_content) + "<h3>Journalists are: </h3>" +"".join(journalists)
-	print(response)
+# # view rendered as httpResponse with html code
+# def home(request):
+# 	articles_title =[]
+# 	articles_content =[]
+# 	journalists =[]
 
-	return HttpResponse(f"<h1>Homepage!</h1><br><p>{response}</p>")
+# 	for a in Article.objects.all():
+# 		articles_title.append("<br>")
+# 		articles_title.append(a.title)
+# 		articles_content.append("<br>")
+# 		articles_content.append(a.content)
+
+# 	for j in Journalist.objects.all():
+# 		journalists.append("<br>")
+# 		journalists.append(j.first_name +" " + j.last_name)
+
+# 	response = "<h3>Article titles are: </h3>" + " ".join(articles_title) + "<h3>Contents are: </h3>"  +  "".join(articles_content) + "<h3>Journalists are: </h3>" +"".join(journalists)
+# 	print(response)
+
+# 	return HttpResponse(f"<h1>Homepage!</h1><br><p>{response}</p>")
 
